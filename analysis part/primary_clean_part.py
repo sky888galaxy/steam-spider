edd4ac7c5e5397bc00c990b7cb92889d88312710
Lenovo <3435575792@qq.com>
2025-10-20 15:11:10 +0800

    Push 排行图

--- analysis part/primary_clean_part.py
+++ analysis part/primary_clean_part.py
@@ -1,10 +1,10 @@
 import csv
 #简单处理
-#给爬虫的数据挨个加上排名以及折扣率,以及把没有数据的位置填上null,到时候分析的时候不会选用这些带null的数据
-def primary_clean(input_file):
-    output_file = "primary_cleaned.csv"
-    with open(input_file, 'r') as uncleaned, \
-            open(output_file, 'w') as cleaned:
+#给清理后的数据挨个加上排名以及折扣率
+def primary_process(input_file):
+    output_file = "data/analysis_use.csv"
+    with open(input_file, 'r',encoding="utf-8") as uncleaned, \
+            open(output_file, 'w',encoding="utf-8") as cleaned:
         reader = csv.reader(uncleaned)
         writer = csv.writer(cleaned)
         newtitle = [h for h in next(reader)]+['rank','discounts']
@@ -12,14 +12,13 @@ def primary_clean(input_file):
         for i,row in enumerate(reader,start=1):
             rank = str(i)
             #是否是免费游戏，不然除数不能为0
-            if row[4] == str(0):
-                discount = 1
+            if row[4] == str(0.0):
+                discount = '100'
             else:
-                discount = str(float(row[3])/float(row[4]))
-            cleaned_row = ['null' if item.strip() == '' else item for item in row]
-            newrow = cleaned_row + [rank,discount]
+                discount = str(round((float(row[3])/float(row[4])),3)*100)
+            newrow = row + [rank,discount]
             writer.writerow(newrow)
 
 # 测试用
-#if __name__ == '__main__':
-    #primary_clean('2.csv')
+if __name__ == '__main__':
+    primary_process('steam_topsellers_simple_cleaned.csv')
